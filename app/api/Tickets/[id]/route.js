a755fc9167bb5db5f106a6905111e37159858f34
Nidhinlal <lalnidhinp02@gmai.com>
2023-11-01 12:31:25 +0530

    Deleting features successfuly

---
+++ app/api/Tickets/[id]/route.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import Ticket from "../../../(models)/Ticket";
+
+
+
+export async function DELETE(req, { params }) {
+    try {
+
+        const { id } = params;
+        await Ticket.findByIdAndDelete(id);
+
+        return NextResponse.json({ message: "Ticket Deleted" }, { status: 200 });
+
+    } catch (error) {
+        return NextResponse.json({ message: "Error", error }, { status: 500 });
+
+    }
+}
--- app/page.js
+++ app/page.js
@@ -2,13 +2,37 @@
 import React from 'react'
 import TicketCard from './(components)/TicketCard'
 
-const Dashboard = () => {
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store"
+    });
+    return res.json();
+  } catch (error) {
+    console.log('Faild to get Tickets', error)
+  }
+}
+
+const Dashboard = async () => {
+  const { tickets } = await getTickets();
+
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category)),
+  ];
   return (
     <div className="p-5">
-      <div className='lg:grid grid-cols-2 xl:grid-cols-4'>
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
+      <div>
+        {tickets && uniqueCategories?.map((uniqueCategorory, categoryIndex) =>
+          <div key={categoryIndex} className='mb-4'>
+            <h2>{uniqueCategorory}</h2>
+            <div className='lg:grid grid-cols-2 xl:grid-cols-4'>
+              {tickets.filter((ticket) => ticket.category === uniqueCategorory).map((filterdTicket, _index) => (
+                <TicketCard id={_index} key={_index} ticket={filterdTicket} />
+              ))}
+            </div>
+          </div>
+        )}
+
       </div>
     </div>
   )
